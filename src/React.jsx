195b4d16d4931569a7f986d9fd04b9cc7e36cdc1
guoxh <guoxh@haier.com>
2023-12-04 21:44:46 +0800

    手写实现useReducer,实现useEffect

--- src/React.jsx
+++ src/React.jsx
@@ -3,32 +3,35 @@ export const root = createRoot(document.getElementById("app"));
 
 const states = [];
 const stateSetters = [];
+const effectDepArr = [];
+
 let stateIndex = 0;
+let effectIndex = 0;
 
 function createState(initialState) {
   return states[stateIndex] ? states[stateIndex] : initialState;
 }
 
 function createStateSetter(stateIndex) {
-  console.log('给每个useState挂载了setState函数')
+  console.log("给每个useState挂载了setState函数");
   return (newState) => {
     if (typeof newState === "function") {
       states[stateIndex] = newState(states[stateIndex]);
     } else {
-      console.log('执行setState函数')
-      states[stateIndex] = newState
+      console.log("执行setState函数");
+      states[stateIndex] = newState;
     }
-    
-    render()
-    
+
+    render();
   };
 }
 // setState执行，需要重新render组件，此时需要把stateIndex重置为0
 // 有疑问？为什么stateIndex重置为0要写在这里.答：重新render之前，将stateIndex初始化
-async function  render(){
-    const App = (await import('./useState')).default
-    stateIndex = 0 
-    root.render(<App/>)
+async function render() {
+  const App = (await import("./useState")).default;
+  stateIndex = 0;
+  effectIndex = 0
+  root.render(<App />);
 }
 export const useState = (initialState) => {
   states[stateIndex] = createState(initialState);
@@ -42,4 +45,31 @@ export const useState = (initialState) => {
   return [_state, _setState];
 };
 
+// 实现useReducer
+export const useReducer = (reducer, initialState) => {
+  const [state, setState] = useState(initialState);
+  const dispatch = (action) => {
+    const newState = reducer(state, action);
+    setState(newState);
+  };
+  return [state, dispatch];
+};
 
+// 实现useEffect
+export const useEffect = (cb, depArr) => {
+  if (typeof cb !== "function") {
+    throw new TypeError("callback must be a function");
+  }
+  if (depArr === undefined) {
+    cb();
+  } else if (!Array.isArray(depArr)) {
+    throw new TypeError("dependencies must be a function");
+  } else {
+    const isChanged = effectDepArr[effectIndex]
+      ? depArr.some((dep, index) => dep !== effectDepArr[effectIndex][index])
+      : true;
+    isChanged && cb();
+    effectDepArr[effectIndex] = depArr;
+    effectIndex++;
+  }
+};
--- src/useState.jsx
+++ src/useState.jsx
@@ -2,18 +2,61 @@
 // const { useState, useReducer, useEffect } = React;
 // const root = createRoot(document.getElementById("app"));
 
-import { root, useState } from './React';
+import { root, useState,useReducer,useEffect } from './React';
+let t;
 function App() {
   const [count, setCount] = useState(0);
   const [count2, setCount2] = useState(0);
+
+  const [count3, dispatch] = useReducer(countReducer, 0);
+  function countReducer(count, { type, payload }) {
+    switch (type) {
+      case "PLUS":
+        return count + payload;
+      case "MINUS":
+        return count - payload;
+      case "TIMES":
+        return count * payload;
+      case "DIV":
+        return count / payload;
+      default:
+        return count;
+    }
+  }
+
+
+  const [second, setSecond] = useState(0);
+  useEffect(() => {
+    // depArr => the Array of dependencies
+    t = setInterval(() => {
+      setSecond((second) => second + 1);
+    }, 10000);
+    //页面卸载时
+    // componentWillUnmount
+    return () => {
+      clearInterval(t);
+      t = null;
+    };
+  }, []);
+  useEffect(() => {
+    setSecond(count);
+  }, [count]);
+
   return (
     <div>
+      {/* useState */}
       <h1>{count}</h1>
       <button onClick={() => setCount(count + 1)}>+</button>
       <button onClick={() => setCount(count - 1)}>-</button>
       <h1>{count2}</h1>
       <button onClick={() => setCount2(count2 + 1)}>+</button>
       <button onClick={() => setCount2(count2=>count2 - 1)}>-</button>
+      {/* useReducer */}
+      <h2>{count3}</h2>
+      <button onClick={() => dispatch({ type: "PLUS", payload: 1 })}>+</button>
+      <button onClick={() => dispatch({ type: "MINUS", payload: 1 })}>-</button>
+      {/* useEffect */}
+      <h2>{second}s</h2>
     </div>
   );
 }
